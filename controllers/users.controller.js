01c8cdcbc043321b487d0c57091139460feafda7
toyshe <toyshehossain@outlook.com>
2024-01-18 12:12:35 +0000

    Adding getUsersByUsername

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,7 +1,16 @@
-const { findUsers } = require("../models/users.model");
+const { findUsers, findUserByUsername } = require("../models/users.model");
 
 exports.getUsers = (req, res, next) => {
   findUsers().then((users) => {
     res.status(200).send({ users });
   });
+};
+
+exports.getUserByUsername = (req, res, next) => {
+  const { username } = req.params;
+  findUserByUsername(username)
+    .then((user) => {
+      res.status(200).send({ user });
+    })
+    .catch(next);
 };
