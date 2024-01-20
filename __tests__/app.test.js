b82914e18d9cd1f18bbed081041e47b4f0574152
toyshe <toyshehossain@outlook.com>
2024-01-20 13:07:18 +0000

    Changing test expected output

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -82,6 +82,7 @@ describe("app", () => {
         body: "I find this existence challenging",
         created_at: "2020-07-09T20:11:00.000Z",
         votes: 100,
+        article_img_url: 'https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700'
       };
       return request(app)
         .get("/api/articles/1")
@@ -116,6 +117,7 @@ describe("app", () => {
         body: "I find this existence challenging",
         created_at: "2020-07-09T20:11:00.000Z",
         votes: 101,
+        article_img_url: 'https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700'
       };
       return request(app)
         .patch("/api/articles/1")
