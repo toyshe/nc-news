5e66448f756c4454cbc21a3a1074f3c3a1fc7316
toyshe <toyshehossain@outlook.com>
2024-02-05 10:00:16 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 
+const cors = require('cors')
+
 const apiRouter = require("./routes/api-router");
 const { errors404, psqlErrors, internalServerError } = require("./error-handling");
 
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 app.use('/api', apiRouter)
 
