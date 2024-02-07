958e0b65e685aece7b8eaf366f84d08dd598f7c3
Toyshe <95750580+toyshe@users.noreply.github.com>
2024-02-07 20:39:18 +0000

    Update article.model.js

--- models/article.model.js
+++ models/article.model.js
@@ -56,8 +56,8 @@ exports.findArticles = (
 };
 
 exports.updateArticleById = (id, body) => {
-  return db
-    .query(`SELECT * FROM articles WHERE article_id=$1`, [id])
+ return db
+    .query(`SELECT articles.*, COUNT(*) AS comment_count FROM articles LEFT JOIN comments ON comments.article_id = articles.article_id WHERE articles.article_id=$1 GROUP BY articles.article_id`, [id])
     .then(({ rows }) => {
       if (rows.length === 0) {
         return Promise.reject({ status: 404, msg: "article_id not found" });
